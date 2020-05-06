e1a416513f9ae696e45f2fb519dac5d2ca4bb88d
Darveen Vijayan <43519646+darveenvijayan@users.noreply.github.com>
2020-05-06 11:41:47 +0800

    Update get_coco.py

--- get_coco.py
+++ get_coco.py
@@ -23,7 +23,7 @@ for im in images:
     print("im: ", im)
     img_data = requests.get(im['coco_url']).content
 
-    with open('/media/darveen/DATADRIVE1/data/yolov3_training_ultralytics/training_prep/downloaded_images/' + im['file_name'], 'wb') as handler:
+    with open('/downloaded_images/' + im['file_name'], 'wb') as handler:
         handler.write(img_data)
 
 
@@ -33,7 +33,7 @@ for im in images:
 
 
 #Download annotations
-with open('/media/darveen/DATADRIVE1/data/yolov3_training_ultralytics/training_prep/annotations_download_' + classes + '.txt', mode='w', newline='') as annot:
+with open('/downloaded_images/annotations_download_' + classes + '.txt', mode='w', newline='') as annot:
 
         for im in images:
                 annIds = coco.getAnnIds(imgIds=im['id'], catIds=catIds, iscrowd=None)
@@ -42,14 +42,14 @@ with open('/media/darveen/DATADRIVE1/data/yolov3_training_ultralytics/training_p
         for i in range(len(anns)):
                 annot_writer = csv.writer(annot)
 
-                annot_writer.writerow(['/media/darveen/DATADRIVE1/data/yolov3_training_ultralytics/training_prep/downloaded_images/' + im['file_name'],
+                annot_writer.writerow(['/downloaded_images/' + im['file_name'],
                         0,
                         round(int(round(anns[i]['bbox'][0]))/im['width'],5), 
                         round(int(round(anns[i]['bbox'][1]))/im['height'],5), 
                         round(int(round(anns[i]['bbox'][3]))/im['width'],5), 
                         round(int(round(anns[i]['bbox'][1]))/im['height'],5)])
 
-                print('/media/darveen/DATADRIVE1/data/yolov3_training_ultralytics/training_prep/downloaded_images/' + im['file_name'],
+                print('/downloaded_images/' + im['file_name'],
                         0,
                         round(int(round(anns[i]['bbox'][0]))/im['width'],5), 
                         round(int(round(anns[i]['bbox'][1]))/im['height'],5), 
